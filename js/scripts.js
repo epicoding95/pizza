371d58e143078526ec156cf7f2ff35e1e919f8b8
Student <example@example.com>
2019-06-21 15:36:53 -0700

    added a nav bar and changed a few colors around

--- js/scripts.js
+++ js/scripts.js
@@ -7,45 +7,34 @@ this.toppings3 = toppings3;
 }
 
 Pizza.prototype.total = function(){
-  var total = this.size + this.toppings1 + this.toppings2 + this.toppings3;
+  var total =  + this.size + this.toppings1 + this.toppings2 + this.toppings3;
   return total;
 }
 
-
+Pizza.prototype.total2 =function(){
+  var total2 = "this is your total" + total;
+  return total2;
+}
 //front end
 
 $(document).ready(function(){
   $("form#YourOrder").submit(function(){
     event.preventDefault();
     var size = parseInt($("select#YourSize").val());
-    console.log(size);
+
     var toppings1 = parseInt($("select#toppings1").val());
-    console.log(toppings1);
+
     var toppings2 = parseInt($("select#toppings2").val());
     var toppings3 = parseInt($("select#toppings3").val());
 
 
 var userPizza = new Pizza (size, toppings1, toppings2, toppings3, total);
-  var total = userPizza.total();
-console.log(userPizza.total);
-
-// debugger;
+  var total =  userPizza.total();
+  var total2 = userPizza.total2();
 
 
-
-$("#output").append(total);
+$("#output").append(total2);
 
 
   });
 });
-
-
-
-
-// var pizza2 = new pizza();
-// pizza.total()
-// var pizza3 = new pizza();
-// pizza.total()
-// var pizza4 = new pizza();
-// pizza.total()
-// pizza.prototype.total = function(total)
