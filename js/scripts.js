f2f66c9c0fe6381fb4c94324c1aad6137d05198d
Student <example@example.com>
2019-06-21 16:10:02 -0700

    everything works except for the delivery option

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //back end
 function Pizza (size,toppings1,toppings2,toppings3){
+this.delivery = delivery
 this.size = size;
 this.toppings1 = toppings1;
 this.toppings2 = toppings2;
@@ -12,7 +13,7 @@ Pizza.prototype.total = function(){
 }
 
 Pizza.prototype.total2 =function(){
-  var total2 = "this is your total" + total;
+  var total2 =  "Your total is: $" + parseInt(this.size + this.toppings1 + this.toppings2 + this.toppings3);
   return total2;
 }
 //front end
@@ -20,20 +21,21 @@ Pizza.prototype.total2 =function(){
 $(document).ready(function(){
   $("form#YourOrder").submit(function(){
     event.preventDefault();
+
+    
     var size = parseInt($("select#YourSize").val());
-
     var toppings1 = parseInt($("select#toppings1").val());
-
     var toppings2 = parseInt($("select#toppings2").val());
     var toppings3 = parseInt($("select#toppings3").val());
 
 
 var userPizza = new Pizza (size, toppings1, toppings2, toppings3, total);
+var userpizza2 = new Pizza (size, toppings1, toppings2, toppings3, total);
   var total =  userPizza.total();
   var total2 = userPizza.total2();
 
 
-$("#output").append(total2);
+$("#output").text(total2);
 
 
   });
