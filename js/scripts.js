f83cd1bc56f0aa7e712e83b91bdc326f46c33997
Student <example@example.com>
2019-06-21 10:34:18 -0700

    added buttons in HTML and starting to add javascript

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function Pizza (size,toppings1,topping2, toppings3){
+this.size = size
+this.toppings1 = toppings1
+this.toppings2 = toppings2
+this.toppings3 = toppings3
+this.total =function(){
+
+  this.total
+  return ("your total is " this.total() ".")
+}
+
+}
+
+var pizza2 = new pizza();
+pizza.total()
+var pizza3 = new pizza();
+pizza.total()
+var pizza4 = new pizza();
+pizza.total()
+pizza.prototype.total = function(total)
+
+
+
+
+
+$(document).ready(function(){
+  $("#yourOrder").submit(function(event){
+    event.preventDefault();
+
+
+var size = $("input#Yoursize").val();
+var toppings1 = $("input#toppings1").val();
+var toppings2 = $("input#toppings2").val();
+var toppings3 = $("input#toppings3").val();
+var total = $("input#Yoursize").val(); + $("input#Yoursize").val(); +
